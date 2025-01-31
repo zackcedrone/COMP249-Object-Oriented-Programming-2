aea833bbe6ad1062aa178403a9295b05e76f0cfd
zackary cedrone <zycemadden@gmail.com>
2025-01-31 14:57:46 -0500

    Add files via upload

--- ClassworkAssignments/Person.java
+++ ClassworkAssignments/Person.java
@@ -2,27 +2,28 @@ public class Person {
 
     private String Name;
     private int Age;
-    private String Address;
+    private Address Address;
 
 
     public Person(){
-        this ("", 0, "");
+        this("", 0, new Address());
     }
 
-    public Person(String Name, int Age, String Address){
+
+    public Person(String Name, int Age, Address Address){
         this.Name=Name;
         this.Age=Age;
         this.Address=Address;
 
     }
 
-    public void setName(){
+    public void setName(String Name){
         this.Name=Name;
     }
-    public void setAge(){
+    public void setAge(int Age){
         this.Age=Age;
     }
-    public void setAddress(){
+    public void setAddress(Address Address){
         this.Address=Address;
     }
     public String getName(){
@@ -31,18 +32,22 @@ public class Person {
     public int getAge(){
         return this.Age;
     }
-    public String getAddress(){
-        return this.Address;
+
+    public Address getAddress(){
+        return new Address(this.Address);
     }
+
     public boolean isEqual(Person other){
-        if (this.Address.equalsIgnoreCase(other.Address) && this.Age==other.Age && this.Name.equalsIgnoreCase(other.Name)){
+        if (this.Address.isEqual(other.Address) && this.Age==other.Age && this.Name.equalsIgnoreCase(other.Name)){
             return true;
         }
         else{
             return false;
         }
     }
-
+    public Person(Person otherPerson){
+        this(otherPerson.Name, otherPerson.Age, new Address(otherPerson.Address));
+    }
     public String toString(){
         return "Name: " + Name + "\nAge: " + Age + "\nAddress: " + Address;
     }
